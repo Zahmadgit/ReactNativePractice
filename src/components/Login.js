8382f1533944c0816592cda413ded28e5943a1b0
Zeshan Ahmad <zahmad5870@gmail.com>
2025-01-20 23:04:32 -0500

    Login page UI finished

---
+++ src/components/Login.js
@@ -0,0 +1,164 @@
+import React, { useState } from 'react'
+import {View, StyleSheet, Text, TextInput, Image, Button, Pressable } from 'react-native'
+import LinearGradient from 'react-native-linear-gradient'
+
+
+const Login =props =>{
+    const[text, setText] = useState('')
+
+    return (
+        <LinearGradient
+        colors={['#bc1434', '#431c3c']} 
+        style={styles.gradient}   
+        start = {{x:0, y: 0.5}}
+        end={{x: 1, y: 0.5}}
+      >
+    <View>
+        <Text style={styles.headerText}>Hello{"\n"}Sign in!</Text>
+            <View style={styles.container}>
+                <Text style={styles.gmail}>Gmail</Text>
+                    <View style={styles.underLine}>
+                        <TextInput 
+                        style={styles.textInput}
+                        onChangeText={setText}
+                        value={text}
+                        placeholder='Enter Email'
+                        />
+                        <Image
+                            source={require('../../assets/checkmark.png')} // Path to your image
+                            style={styles.checkmark}
+                        />
+                    </View>
+                    <Text style={styles.password}>Password</Text>
+                    <View style={styles.underLine}>
+                        <TextInput style={styles.textInput} secureTextEntry={true}>Password</TextInput>
+                        <Image
+                            source={require('../../assets/crossedeye.png')} // Path to your image
+                            style={styles.checkmark}
+                        />
+                    </View>
+                    <Text style={styles.forgot}>Forgot password?</Text>
+
+                    
+                       
+                    <Pressable style={styles.button}>
+                        <LinearGradient
+                            colors={['#bc1434', '#431c3c']}  // Gradient colors from purple to pink
+                            style = {styles.button}
+                            start = {{x:0, y: 0.5}}
+                            end={{x: 1, y: 0.5}}
+                        >
+                            <Text style={styles.signIn}>SIGN IN</Text>
+                        </LinearGradient>
+                    </Pressable>
+                    
+                    <Text style={styles.dont}>Don't have account?</Text>
+                    <Text style={styles.signup}>Sign up</Text>
+                  
+            </View> 
+            
+            
+    </View>
+    </LinearGradient>
+    )
+}
+
+const styles = StyleSheet.create({
+    signup:{
+        
+        textAlign: 'right',
+        paddingRight: '10%',
+        color: 'black',
+        fontSize: 19,
+        fontWeight: '900'
+    },
+    dont:{
+        marginTop: '55%',
+        textAlign: 'right',
+        paddingRight: '10%',
+        color: 'grey',
+        fontSize: 16,
+        fontWeight: '500'
+    },
+    signIn:{
+        fontSize: 22,
+        color: 'white',
+        textAlign: 'center',
+        paddingTop: '3%',
+        fontWeight: 'bold'
+
+    },
+    button:{
+        
+        height: 50,
+        width: 300,
+        alignSelf: 'center',
+        borderRadius: 25,
+        marginTop: '20%'
+        
+    },
+    forgot:{
+        fontSize: 16,
+        paddingTop: '6%',
+        textAlign: 'right',
+        paddingRight: '10%',
+        fontWeight: 'bold'
+    },
+    container: {
+        height: '85%',
+        backgroundColor: 'white',
+        borderTopLeftRadius: 45,
+        borderTopRightRadius: 45
+    },
+    gradient: {
+        height: "100%"
+    },
+    headerText: {
+        color: '#fff',
+        fontSize: 26,
+        fontWeight: 'bold',
+        padding: '10%',
+    },
+    gmail:{
+        color: '#bc1434',
+        fontSize: 16,
+        fontWeight: 'bold',
+        paddingLeft: '10%',
+        paddingTop: '20%',
+
+    },
+    password:{
+        color: '#bc1434',
+        fontSize: 16,
+        fontWeight: 'bold',
+        paddingLeft: '10%',
+        paddingTop: '5%',
+        
+
+    },
+    underLine: {
+        borderBottomWidth: 1,
+        borderBottomColor: 'grey',
+        width: '80%',
+        textAlign: 'left',
+        alignSelf: 'center',
+        flexDirection: 'row',
+    },
+      textInput: {
+        flex: 1, // TextInput takes up remaining space
+        padding: -10,
+        fontSize: 16,
+      },
+      checkmark: {
+        width: 25,
+        height: 25,
+        marginLeft: '10%', // Space between the input and the image
+        
+      },
+
+    
+    
+})
+
+
+export default Login
