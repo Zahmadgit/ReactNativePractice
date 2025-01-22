4a9687774cc6417278b8e4a23b5b9d4e3f349ffb
Zeshan Ahmad <zahmad5870@gmail.com>
2025-01-22 09:23:55 -0500

    asynchstorage

--- src/screens/AsynStorageExample.js
+++ src/screens/AsynStorageExample.js
@@ -9,7 +9,8 @@ const AsynStorageExample = props =>{
         
         const saveData = async () => {
             try {
-                await AsyncStorage.setItem('key', input);
+                
+                await AsyncStorage.setItem('key', JSON.stringify(input));
                 alert('Data saved successfully!');
             } catch (error) {
                 alert( error);
@@ -18,6 +19,7 @@ const AsynStorageExample = props =>{
         const loadData = async () => {
             try {
                 const value = await AsyncStorage.getItem('key');
+                
                 if (value !== null) {
                     setStoredValue(value);
                     alert(value)
@@ -28,24 +30,89 @@ const AsynStorageExample = props =>{
                 alert(error);
             }
         };
+        const removeValue = async () => {
+            try {
+              await AsyncStorage.removeItem('key')
+            } catch(e) {
+              // remove error
+            }
+          
+            console.log('Done.')
+          }
 
+
+
+          const USER_1 = {
+            name: 'Tom',
+            age: 20,
+            traits: {
+              hair: 'black',
+              eyes: 'blue'
+            }
+          }
+          
+          const USER_2 = {
+            name: 'Sarah',
+            age: 21,
+            hobby: 'cars',
+            traits: {
+              eyes: 'green',
+            }
+          }
+          const getAllKeys = async () => {
+            try {
+              const keys = await AsyncStorage.getAllKeys();
+              alert(`All Keys: ${keys.join(', ')}`);
+            } catch (error) {
+              console.error('Error getting all keys:', error);
+            }
+          };
+          
+          const mergeUsers = async () => {
+            try {
+              //save first user
+              await AsyncStorage.setItem('@MyApp_user', JSON.stringify(USER_1))
+          
+              // merge USER_2 into saved USER_1
+              await AsyncStorage.mergeItem('@MyApp_user', JSON.stringify(input))
+          
+              // read merged item
+              const currentUser = await AsyncStorage.getItem('@MyApp_user')
+          
+              console.log(currentUser)
+          
+              // console.log result:
+              // {
+              //   name: 'Sarah',
+              //   age: 21,
+              //   hobby: 'cars',
+              //   traits: {
+              //     eyes: 'green',
+              //     hair: 'black'
+              //   }
+              // }
+            } catch (e){
+
+            }
+          } 
 
         return(
             <View>
-      <Text style={styles.header}>AsyncStorage Example</Text>
-      <View style={styles.row}>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter something..."
-          value={input}
-          onChangeText={setInput}
-        />
-        <Button title="Save" onPress={saveData} />
-        <Button title="Load" onPress={loadData} />
-        
-      </View>
-      <Text style={styles.result}>Stored Value: {storedValue}</Text>
-    </View>
+                <Text style={styles.header}>AsyncStorage Example</Text>
+                <View style={styles.row}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Enter something..."
+                    value={input}
+                    onChangeText={setInput}
+                />
+                <Button title="Save" onPress={saveData} />
+                <Button title="Load" onPress={loadData} />
+                <Button title="Remove" onPress={removeValue}/>
+                <Button title="Get All Keys" onPress={getAllKeys} />
+                </View>
+                <Text style={styles.result}>Stored Value: {storedValue}</Text>
+            </View>
         )
 }
 
